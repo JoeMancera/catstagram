4ec67513b20387d4651bfc55a0bdc35d7b02a4d4
JoeMancera <joeharrymancera@gmail.com>
2021-07-13 20:02:56 -0500

    Was added hooks in the app file and test with rick and morty app

--- src/components/CardVote.jsx
+++ src/components/CardVote.jsx
@@ -3,12 +3,11 @@ import '../assets/styles/components/CardVote.css'
 import likeButton from '../assets/static/like-button.png';
 import dislikeButton from '../assets/static/dislike-button.png';
 import favoriteButton from '../assets/static/Fav-button.png';
-import catImage from '../assets/static/cat_1.jpg';
 
-const CardVote = () => (
+const CardVote = ({ urlImage }) => (
   <div className='card_vote'>
     <figure>
-      <img className='cat_image' src={catImage} alt='Cat' srcSet='' />
+      <img className='cat_image' src={urlImage} alt='Cat' srcSet='' />
       <div className='card_vote_actions'>
         <button type='button' className='btn-card'>
           <img src={likeButton} alt='Like button' />
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Section from '../components/Sections';
 import CardVote from '../components/CardVote';
 import '../assets/styles/App.css';
 
-const App = () => (
-  <div>
-    <Header />
-    <main>
-      <Section title='Cats of the day'>
-        <CardVote />
-      </Section>
-    </main>
-  </div>
-);
+const App = () => {
+  const [cats, setCats] = useState({ results: [] });
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((data) => setCats(data));
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <Section title='Cats of the day'>
+          { cats.results.map((item) => <CardVote urlImage={item.image} />)}
+        </Section>
+      </main>
+    </div>
+  );
+};
 
 export default App;
