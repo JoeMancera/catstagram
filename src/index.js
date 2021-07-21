f70c4d3a8010bc9f24da36d61d949d064f373a4b
JoeMancera <joeharrymancera@gmail.com>
2021-07-20 20:17:33 -0500

    was fixed dependencies problem

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
-import reducer from './reducers';
 
 import App from './containers/App';
 
 const rootElement = document.getElementById('root');
-const store = createStore(reducer, []);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   rootElement,
 );
