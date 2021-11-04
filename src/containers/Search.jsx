fc1f8f2c5cdaf0d1ac854c5f0551454175e718d8
JoeMancera <joeharrymancera@gmail.com>
2021-11-04 10:06:36 -0500

    Was fixed the searchcontainer logic

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Section from '../components/Sections';
 import CardLoading from '../components/CardLoading';
 import SearchForm from '../components/SearchForm';
 import CardSearch from '../components/CardSearch';
-import useSearchCats from '../hooks/useSearchCats';
+import dummycats from '../utils/dummyCats';
 
 const API = process.env.REACT_APP_CATS_API_URL;
 const API_KEY = process.env.REACT_APP_CATS_API_KEY;
@@ -11,16 +12,28 @@ const API_KEY = process.env.REACT_APP_CATS_API_KEY;
 const Search = () => {
 
   const [category, setCategy] = useState('');
-  const { cats, error } = useSearchCats();
+  const [cats, setCats] = useState(dummycats);
+  const [error, setError] = useState('');
 
   const handleChangeCategory = (event) => {
     setCategy(event.target.value);
   };
 
-  const handlerClickSearchCategory = () => {
-    const { cats, error } = useSearchCats(`${API}/images/search`, API_KEY, category);
-    setCats(cats);
-    setError(error);
+  const handlerClickSearchCategory = async () => {
+    const config = {
+      headers: {
+        'x-api-key': API_KEY,
+      },
+    };
+
+    try {
+      const result = await axios.get(`${API}/images/search?limit=12&category=${category}`, config);
+      console.log(result);
+      setCats(result.data);
+
+    } catch (error) {
+      setError(error);
+    }
   };
 
   return (
--- src/hooks/useGetBreed.js
+++ src/hooks/useGetBreed.js
@@ -5,21 +5,25 @@ const useGetBreed = (API, API_KEY) => {
   const [breedOfTheDay, setBreedOfTheDay] = useState([]);
   const [error, setError] = useState('');
 
-  const config = {
-    headers: {
-      'x-api-key': API_KEY,
-    },
-  };
+  useEffect(() => {
 
-  useEffect(async () => {
-    try {
-      const result = await axios(API, config);
-      setBreedOfTheDay(result.data[Math.floor(Math.random() * result.data.length)]);
-    } catch (error) {
-      console.log(error);
-      setError(error);
+    const config = {
+      headers: {
+        'x-api-key': API_KEY,
+      },
+    };
+
+    async function fetchData() {
+      try {
+        const result = await axios(API, config);
+        setBreedOfTheDay(result.data[Math.floor(Math.random() * result.data.length)]);
+      } catch (error) {
+        console.log(error);
+        setError(error);
+      }
     }
-  }, []);
+    fetchData();
+  }, [API, API_KEY]);
 
   return { breedOfTheDay, error };
 };
